087c019632882fcf43a154f49e7d18889feb4f7f
Amoylee49 <Amoyleeo@qq.com>
2023-02-28 07:46:50 +0800

    test

--- puppeteerServer.js
+++ puppeteerServer.js
@@ -1,26 +1,6 @@
 const express = require('express')
 const puppeteer = require('puppeteer')
 const app = express()
-const contentType = {
-    "css": "text/css",
-    "gif": "image/gif",
-    "html": "text/html",
-    "ico": "image/x-icon",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "js": "text/javascript",
-    "json": "application/json",
-    "pdf": "application/pdf",
-    "png": "image/png",
-    "svg": "image/svg+xml",
-    "swf": "application/x-shockwave-flash",
-    "tiff": "image/tiff",
-    "txt": "text/plain",
-    "wav": "audio/x-wav",
-    "wma": "audio/x-ms-wma",
-    "wmv": "video/x-ms-wmv",
-    "xml": "text/xml"
-}
 app.get('/puppe', (req, res) => {
     let query = req.query;
     // let query = eval(req.query);
